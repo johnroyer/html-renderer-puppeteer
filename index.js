c53e766e2ce95e6c24b9f889616009de3484fc68
Zero <john.royer@gmail.com>
2024-08-27 14:19:42 +0800

    add example script to get HTML

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Thank you for using WebStorm 💙')
+import puppeteer from 'puppeteer'
+
+const browser = await puppeteer.launch()
+
+const page = await browser.newPage()
+
+await page.goto('https://zeroplex.tw/ip')
+
+await page.content()
+    .then(function(content) {
+        console.log("----------")
+        console.log(content)
+        console.log("----------")
+    })
+    .then(function (failure) {
+        console.log(failure)
+})
+
+browser.close()
