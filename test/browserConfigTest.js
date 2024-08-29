9ac18c3081d339c17abddd5d5bf8d78180aeeca8
Zero <john.royer@gmail.com>
2024-08-29 11:37:32 +0800

    reformat code

--- test/browserConfigTest.js
+++ test/browserConfigTest.js
@@ -2,28 +2,28 @@ import BrowserConfig from '../browserConfig.js'
 import assert from 'assert'
 
 describe('getProxyProtocol', function () {
-    it ('should be HTTP', function () {
+    it('should be HTTP', function () {
         assert.equal(
             BrowserConfig.getProxyProtocol('http://127.0.0.1'),
             'http:',
         )
     })
 
-    it ('should be HTTPS', function () {
+    it('should be HTTPS', function () {
         assert.equal(
             BrowserConfig.getProxyProtocol('https://127.0.0.1'),
             'https:',
         )
     })
 
-    it ('should be SOCKET', function () {
+    it('should be SOCKET', function () {
         assert.equal(
             BrowserConfig.getProxyProtocol('socket5://127.0.0.1'),
             'socket5:'
         )
     })
 
-    it ('should throws error', function () {
+    it('should throws error', function () {
         assert.equal(
             BrowserConfig.getProxyProtocol('invalid://127.0.0.1'),
             'invalid:'
