27dfa0fd689879f7bff50a59640341217c7e2484
Zero <john.royer@gmail.com>
2024-08-29 09:42:22 +0800

    parse HTTP POST data

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import http from "http"
+import queryString from "querystring";
 
 const address = '127.0.0.1'
 const port = 38080
 
 const httpServer = http.createServer((req, res) => {
     if (req.url=='/') {
+        let body = ''
+        let postData
+
+        req.on('data', (data) => {
+            body += data
+        })
+
+        req.on('end', () => {
+            postData = queryString.parse(body)
+
+            console.log(postData)
+        })
+
         res.write('hello world')
         res.end()
     }
