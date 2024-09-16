8b515401a852a4ff588375718bc315c582d49a23
Zero <john.royer@gmail.com>
2024-09-16 12:46:02 +0800

    rename variable

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ import Renderer from "./renderer.js";
 const address = '127.0.0.1'
 const port = 38080
 
-const httpServer = http.createServer((req, res) => {
-    if (req.url=='/') {
+const httpServer = http.createServer((request, response) => {
+    if (request.url=='/') {
         let body = ''
         let data
 
-        req.on('data', (data) => {
+        request.on('data', (data) => {
             body += data.toString()
         })
 
-        req.on('end', () => {
+        request.on('end', () => {
             data = JSON.parse(body)
 
             let renderer = new Renderer(data)
             let result = renderer.run()
 
             result.then(function (back) {
-                res.end(JSON.stringify(back))
+                response.end(JSON.stringify(back))
             })
 
         })
     } else {
-        res.writeHead(404)
-        res.end()
+        response.writeHead(404)
+        response.end()
     }
 })
 httpServer.listen(port, address)
